c67020c815bbdcf845bfb2c9762d60069010a9ac
Vidisha8904 <116243721+Vidisha8904@users.noreply.github.com>
2025-02-10 13:48:55 +0000

    model change

--- rag.py
+++ rag.py
@@ -60,7 +60,7 @@ def get_conversational_chain():
     Answer:
     """
 
-    model = ChatOpenAI(model="gpt-4",
+    model = ChatOpenAI(model="gpt-4o",
                        temperature=0.3)
 
     prompt = PromptTemplate(template = prompt_template, input_variables = ["context", "question"])
